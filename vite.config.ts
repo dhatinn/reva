d0deab75dc60212e5cf152220f39c8763504fb0f
dhatinn <dhatinsandy24@gmail.com>
2026-05-12 12:32:33 +0700

    fix: simpan perubahan sebelum pull

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 export default defineConfig({
   base: '/reva/', // Ganti 'reva' dengan nama repo GitHub kamu
   plugins: [react()],
